6632dce913374c0aec9d20f4655dd928458d57a7
mauneven <mauneven@gmail.com>
2026-02-12 19:39:22 -0500

    Initial Commit

---
+++ Sentinel/SentinelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelApp.swift
+//  Sentinel
+//
+//  Created by mauneven on 12/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SentinelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
